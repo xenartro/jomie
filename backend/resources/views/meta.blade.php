2479570679af572691d0c11def7481d38625bd2f
Matias Chomicki <matias.chomicki@gmail.com>
2025-03-08 15:32:21 +0100

    Meta: add missing content property

--- backend/resources/views/meta.blade.php
+++ backend/resources/views/meta.blade.php
@@ -6,10 +6,14 @@
 <meta property="og:url" content="{{ $user->url }}" />
 <meta property="og:title" content="{{ $user->preferences->meta_title }}" />
 <meta property="og:description" content="{{ $user->preferences->meta_description }}" />
+@if (!empty($content['basic']['profile_image']))
 <meta property="og:image" content="{{ asset('/storage/images/'.$content['basic']['profile_image']) }}" />
+@endif
 
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="{{ $user->url }}" />
 <meta property="twitter:title" content="{{ $user->preferences->meta_title }}" />
 <meta property="twitter:description" content="{{ $user->preferences->meta_description }}" />
+@if (!empty($content['basic']['profile_image']))
 <meta property="twitter:image" content="{{ asset('/storage/images/'.$content['basic']['profile_image']) }}" />
+@endif
